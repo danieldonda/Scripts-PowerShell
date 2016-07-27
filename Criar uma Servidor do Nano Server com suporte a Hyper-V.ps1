e3f4a38d29cd4311bb1549305a4c1f3f2a46168b
Daniel Donda <danieldonda@users.noreply.github.com>
2016-07-27 09:04:23 -0300

    Nano Server com suporte a Hyper-V

--- Criar uma Servidor do Nano Server com suporte a Hyper-V.ps1
+++ Criar uma Servidor do Nano Server com suporte a Hyper-V.ps1
@@ -14,18 +14,3 @@ New-NanoServerImage -EnableRemoteManagementPort `
  -Package Microsoft-NanoServer-Compute-Package `
  -AdministratorPassword (ConvertTo-SecureString -String ‘P@ssw0rd’ -AsPlainText -Force)
 
-
- $VM = 'NanoServer Hyper-v Server'
-Set-VMProcessor -VMName $VM -ExposeVirtualizationExtensions $true
-Set-VMMemory -VMName $VM -DynamicMemoryEnabled $false
-Get-VMNetworkAdapter -VMName $VM | Set-VMNetworkAdapter -MacAddressSpoofing On
-
-#Criar a REDE
-$NanoServerName = "NanoHV"
-$cred = Get-Credential 'Administrator'
-$s = New-PSSession -ComputerName $NanoServerName -Credential $cred
-Enter-PSSession -Session $s
-$ethernet = Get-NetAdapter -Name ethernet
-New-VMSwitch -Name externalSwitch -NetAdapterName $ethernet.Name -AllowManagementOS $true
-
-#Depois basta logar no NanoServer
